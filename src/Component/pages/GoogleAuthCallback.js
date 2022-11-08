791353116f817e3ba3eb84e7636813b2abfc0986
nikiaprian <aprianniki69@gmail.com>
2022-11-08 12:34:45 +0700

    second commit

--- src/Component/pages/GoogleAuthCallback.js
+++ src/Component/pages/GoogleAuthCallback.js
@@ -11,7 +11,7 @@ function GoogleAuthCallback() {
   useEffect(() => {
     const { search } = location;
     axios
-      .get(`http://35.78.193.13:9090/auth/callback/google${search}`, {
+      .get(`http:/35.79.180.12:9090/auth/callback/google${search}`, {
         withCredentials: false,
       })
       .then((res) => {
